d0d5f9dbf0c4b7139168cad74d5d04c34efce490
Your Name <you@example.com>
2022-11-16 13:58:04 +0100

    modified:   logowanie.php

--- logowanie.php
+++ logowanie.php
@@ -1,5 +1,15 @@
 <?php
-echo "chuj";
-echo "chuj2";
+class User{
+    private int $id;
+    private string $login;
+    private string $password;
+    private string $firstname;
+    private string $lastname;
+    public function __construct(string $login, string $password)
+    {
+        $this ->login = $login;
+        $this->password = $password;
+    }
+}
 
 ?>
