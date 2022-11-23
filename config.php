4141b059a156ccb4098e8e6fa6b7828e518bed6f
Your Name <you@example.com>
2022-11-23 13:20:50 +0100

    new file:   config.php 	new file:   test.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+require_once('vendor/autoload.php');
+//loader to taki pomocnik do ładowania szablonów
+$loader = new Twig\Loader\FilesystemLoader('templates');
+//inicjuje twiga
+$twig = new Twig\Environment($loader)
+
+$db = new mysqli('localhost', 'root', '', 'loginform');
